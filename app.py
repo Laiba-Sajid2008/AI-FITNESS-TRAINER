57cb09c5cf2103aa4f5e7c4428b6f319700e3ef2
Laiba-Sajid2008 <laibasajid3210@gmail.com>
2026-04-15 03:38:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -350,19 +350,7 @@ with cam_col:
     webrtc_streamer(
         key="fitness-tracker",
         video_processor_factory=FitnessProcessor,
-        rtc_configuration=RTCConfiguration({"iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]},
-            {
-                "urls": ["turn:openrelay.metered.ca:80"],
-                "username": "openrelayproject",
-                "credential": "openrelayproject",
-            },
-            {
-                "urls": ["turn:openrelay.metered.ca:443"],
-                "username": "openrelayproject",
-                "credential": "openrelayproject",
-            },
-        ]}),
+        rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}, {"urls": ["stun:stun1.l.google.com:19302"]}, {"urls": ["stun:stun2.l.google.com:19302"]}]}),
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True,
     )
